f758b702a7dd4870b1dc2fca8fd4df009b91b5c0
Federico Ferri <federico.ferri.it@gmail.com>
2023-01-19 16:48:11 +0100

    doc getMesh()

--- simIGL.lua
+++ simIGL.lua
@@ -1,7 +1,10 @@
 local simIGL={}
 
+--@fun getMesh get mesh data of a given shape in the format used by simIGL functions
+--@arg int h the handle of the shape
+--@arg {type=table,default={}} options options
+--@ret table mesh mesh object
 function simIGL.getMesh(h,options)
-    options=options or {}
     local v,i,n=sim.getShapeMesh(h)
     local m=sim.getObjectMatrix(h,-1)
     v=sim.multiplyVector(m,v)
