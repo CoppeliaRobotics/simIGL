67642bd3a65619a49bdd120ef1cd77717a52da23
Federico Ferri <federico.ferri.it@gmail.com>
2023-01-19 15:20:23 +0100

    + getMesh()

--- simIGL.lua
+++ simIGL.lua
@@ -1,3 +1,11 @@
 local simIGL={}
 
+function simIGL.getMesh(h,options)
+    options=options or {}
+    local v,i,n=sim.getShapeMesh(h)
+    local m=sim.getObjectMatrix(h,-1)
+    v=sim.multiplyVector(m,v)
+    return {vertices=v, indices=i}
+end
+
 return simIGL
