12ce4514efca0526641ff8fa67c403bdf6ad6102
Federico Ferri <federico.ferri.it@gmail.com>
2023-02-15 12:21:50 +0100

    optimize for multiple args & simplify code

--- simIGL.lua
+++ simIGL.lua
@@ -17,32 +17,17 @@ end
 --@ret int handleResult the handle of the resulting shape
 function simIGL.meshBooleanShape(handles,op)
     if #handles<2 then error('not enough input shapes') end
-    local function compute(a,b)
-        local function blendColor(a,b) return (0.5*(Vector(a)+Vector(b))):data() end
-        local m=simIGL.meshBoolean(simIGL.getMesh(a),simIGL.getMesh(b),op)
-        local edgesA=sim.getObjectInt32Param(a,sim.shapeintparam_edge_visibility)
-        local edgesB=sim.getObjectInt32Param(b,sim.shapeintparam_edge_visibility)
-        local h=sim.createMeshShape(1+2*edgesA*edgesB,math.pi/8,m.vertices,m.indices)
-        local _,coladA=sim.getShapeColor(a,nil,sim.colorcomponent_ambient_diffuse)
-        local _,coladB=sim.getShapeColor(b,nil,sim.colorcomponent_ambient_diffuse)
-        sim.setShapeColor(h,nil,sim.colorcomponent_ambient_diffuse,blendColor(coladA,coladB))
-        local _,colspA=sim.getShapeColor(a,nil,sim.colorcomponent_specular)
-        local _,colspB=sim.getShapeColor(b,nil,sim.colorcomponent_specular)
-        sim.setShapeColor(h,nil,sim.colorcomponent_specular,blendColor(colspA,colspB))
-        local _,colemA=sim.getShapeColor(a,nil,sim.colorcomponent_emission)
-        local _,colemB=sim.getShapeColor(b,nil,sim.colorcomponent_emission)
-        sim.setShapeColor(h,nil,sim.colorcomponent_emission,blendColor(colemA,colemB))
-        return h
-    end
-    local result=compute(handles[1],handles[2])
-    if #handles>2 then
-        local toRemove={}
-        for i=3,#handles do
-            table.insert(toRemove,result)
-            result=compute(result,handles[i])
-        end
-        sim.removeObjects(toRemove)
-    end
+    local meshes=map(simIGL.getMesh,handles)
+    local result=simIGL.meshBoolean(meshes[1],meshes[2],op)
+    for i=3,#meshes do result=simIGL.meshBoolean(result,meshes[i],op) end
+    local edges=sim.getObjectInt32Param(handles[1],sim.shapeintparam_edge_visibility)
+    result=sim.createMeshShape(1+2*edges,math.pi/8,result.vertices,result.indices)
+    local _,colad=sim.getShapeColor(handles[1],nil,sim.colorcomponent_ambient_diffuse)
+    sim.setShapeColor(result,nil,sim.colorcomponent_ambient_diffuse,colad)
+    local _,colsp=sim.getShapeColor(handles[1],nil,sim.colorcomponent_specular)
+    sim.setShapeColor(result,nil,sim.colorcomponent_specular,colsp)
+    local _,colem=sim.getShapeColor(handles[1],nil,sim.colorcomponent_emission)
+    sim.setShapeColor(result,nil,sim.colorcomponent_emission,colem)
     sim.reorientShapeBoundingBox(result,sim.handle_world)
     return result
 end
