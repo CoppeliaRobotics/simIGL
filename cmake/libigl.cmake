88a6ce73f94f932c36edb1183f6e308525b7a5ed
Federico Ferri <federico.ferri.it@gmail.com>
2024-06-20 09:38:37 +0200

    exclude libigl installs

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -7,5 +7,13 @@ FetchContent_Declare(
     libigl
     GIT_REPOSITORY https://github.com/libigl/libigl.git
     GIT_TAG v2.5.0
+    #EXCLUDE_FROM_ALL # only available in CMake 3.28+
 )
-FetchContent_MakeAvailable(libigl)
+#FetchContent_MakeAvailable(libigl) # CMake 3.28+
+
+# workaround for CMake < 3.28, to add EXCLUDE_FROM_ALL:
+FetchContent_GetProperties(libigl)
+if(NOT libigl_POPULATED)
+  FetchContent_Populate(libigl)
+  add_subdirectory(${libigl_SOURCE_DIR} ${libigl_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
